e49a208238ce10c337e8e67093b487ce51a1d4b9
PSiHachina <psihachina@gmail.com>
2020-02-07 12:24:11 +0300

    add models

---
+++ models/ViewModels.js
@@ -0,0 +1,16 @@
+exports.Task = class {
+
+    constructor(title, start, end, status) {
+        this.title = title || '';
+        this.startDate = start || new Date;
+        this.endDate = end || new Date;
+        this.status = status || '';
+    }
+}
+
+exports.MainViewModel = class {
+    constructor(title, tasks) {
+        this.title = title || '';
+        this.tasks = tasks || [];
+    }
+}
