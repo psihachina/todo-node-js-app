af540552b4363c4b96712c54cbd005fa61c80588
PSiHachina <psihachina@gmail.com>
2020-02-28 10:15:21 +0300

    1

--- routes/main.js
+++ routes/main.js
@@ -2,7 +2,7 @@ const { MainViewModel } = require('../models');
 const db = require('../utils/sqlitedb');
 
 module.exports = (r, q) => {
-    db.getTasks().then(task => {
+    db.getTasks().then(task => 
         db.getStatuses().then(statuses => {
             let model = new MainViewModel('TODO LIST', task, statuses);
             q.render('index', model);
